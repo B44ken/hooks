faec3b49c4627417d43fea4fc1f95bf790f7b49a
b44ken <bradboratto@gmail.com>
2023-11-01 16:41:42 -0400

    github api stuff

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ def update_from_file():
     config = json.load(open('hooks.json'))
     for hook in config:
         if hook['service'] == 'github':
+            last_updated = service_github.last_updated(hook['repo'])
             last_run = hook.get('lastRun', 0)
             if service_github.last_updated(hook['repo']) < last_run:
                 continue
@@ -15,11 +16,11 @@ def update_from_file():
             hook['lastRun'] = int(time.time())
             try:
                 os.system(hook['command'])
+                json.dump(config, open('hooks.json', 'w'), indent=4)
             except:
                 print(f'error running {hook["command"]}')
-            json.dump(config, open('hooks.json', 'w'), indent=4)
 
 if __name__ == '__main__':
     while True:
         update_from_file()
-        time.sleep(10)
+        time.sleep(15)
--- src/service_github.py
+++ src/service_github.py
@@ -1,10 +1,14 @@
-import requests, time
+import requests, time, os
+
+GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
 
 def last_updated(repo):
     url = f'https://api.github.com/repos/{repo}'
-    response = requests.get(url)
+    response = requests.get(url, headers={'Authorization': f'token {GITHUB_TOKEN}'})
     if response.status_code == 200:
-        updated = response.json()['updated_at']
-        return time.mktime(time.strptime(updated, '%Y-%m-%dT%H:%M:%SZ'))
+        updated = response.json()['pushed_at']
+        t = time.mktime(time.strptime(updated, '%Y-%m-%dT%H:%M:%SZ'))
+        # eastern standard time hack, fix asap
+        return int(t) - 60*60*4
     else:
         return None
